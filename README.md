aaf0d83db1773c75f6e4305c755678c6901b388f
Heather <heather@cynede.net>
2013-08-22 10:44:04 +0400

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+GHCS
+====
+
+Light thing to make shell scripting on haskell neat.
+
+originally came from StackOverflow with
+
+ - lesser improvements
+ - move to up to date haskell platform
+ - windows support
+
+``` haskell
+when compile $ do
+     r <- system $ "ghc --make " ++ scr
+     case r of
+       ExitFailure i > do
+               hPutStrLn stderr $ "'ghc --make " ++ scr ++ "' failed: " ++ show i
+               exitFailure
+       ExitSuccess > return ()
+```
+ 
+``` shell
+$ time hs-echo.hs "Hello, world\!"     
+[1 of 1] Compiling Main             ( hs-echo.hs, hs-echo.o )
+Linking hs-echo ...
+Hello, world!
+hs-echo.hs "Hello, world!"  0.83s user 0.21s system 97% cpu 1.062 total
+
+$ time hs-echo.hs "Hello, world, again\!"
+Hello, world, again!
+hs-echo.hs "Hello, world, again!"  0.01s user 0.00s system 60% cpu 0.022 total
+```
+
+``` shell
+C:\test>t GHCS test.hs
+[1 of 1] Compiling Main             ( test.hs, test.o )
+Linking test.exe ...
+hello world
+command took 0:0:0.75 (0.75s total)
+
+C:\test>t GHCS test.hs
+hello world
+command took 0:0:0.13 (0.13s total)
+```
---
+++ src/GHCS.hs
@@ -0,0 +1,52 @@
+﻿{-# LANGUAGE UnicodeSyntax, CPP #-}
+
+import Control.Monad
+
+import System.Environment( getArgs )
+import System.Exit
+import System.Directory
+import System.IO
+
+#if ! ( defined(mingw32_HOST_OS) || defined(__MINGW32__) )
+import System.Posix.Process
+import System.Posix.Files
+#endif
+
+import System.Process
+
+#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+#else
+getMTime f = getFileStatus f >>= return . modificationTime
+#endif
+
+main = do
+    scr : args <- getArgs
+    let cscr = takeWhile (/= '.') scr
+
+    scrExists <- doesFileExist scr
+    cscrExists <- doesFileExist cscr
+    compile <- if scrExists && cscrExists
+               then do
+#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+                 return False
+#else
+                 scrMTime <- getMTime scr
+                 cscrMTime <- getMTime cscr
+                 return $ cscrMTime <= scrMTime
+#endif
+               else return True
+
+    when compile $ do
+         r <- system $ "ghc --make " ++ scr
+         case r of
+           ExitFailure i → do
+                   hPutStrLn stderr $ "'ghc --make " ++ scr ++ "' failed: " ++ show i
+                   exitFailure
+           ExitSuccess → return ()
+           
+#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+    pid <- runCommand cscr
+    waitForProcess pid >>= exitWith
+#else
+    executeFile cscr False args Nothing
+#endif
